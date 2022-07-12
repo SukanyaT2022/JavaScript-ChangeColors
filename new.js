b917c5960cab667a654205110a3df1f0e23e4fd4
Tiparpron Sukanya <tiparpron@Tiparprons-MacBook-Air.local>
2022-07-12 20:26:16 +0700

    first commit

---
+++ new.js
@@ -0,0 +1,4 @@
+const click=document.getElementById("new")
+click.style.backgroundColor='green';
+
+document.getElementById("new").style.color='red';
---
+++ psge.js
@@ -0,0 +1,6 @@
+//const test1 = document.getElementById('clickMe');
+//test1.style.backgroundColor='pink';
+
+//https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+
+document.getElementById('clickMe').style.color = 'red';
